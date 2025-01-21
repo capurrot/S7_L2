e6f6d7ecb636eb9983cb003f36a4f3a3fe659bb8
Timoteo Capurro <timoteo.capurro@infosyscap.net>
2025-01-21 15:59:45 +0000

    correzione counter

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,6 +27,8 @@ newP.classList.add("text-center");
 let counter = 0;
 if (sessionStorage.getItem("count")) {
   counter = parseInt(sessionStorage.getItem("count"));
+} else {
+  counter = -1;
 }
 addCount();
 function addCount() {
